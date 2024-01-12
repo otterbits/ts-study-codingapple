43e250363e501163e198815c4305971993a591ed
uoohoon <hoonixox@naver.com>
2024-01-12 15:46:26 +0900

    init ts-study-codingapple

---
+++ index.js
@@ -0,0 +1,12 @@
+var 이름 = "otter";
+var 나이 = 29;
+var 출생지역 = "진주";
+var favor = {
+    song: "그대 입술이",
+    singer: "장범준",
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,14 @@
+let 이름: string = "otter";
+let 나이: number = 29;
+let 출생지역: string = "진주";
+
+let favor: { song: string; singer: string } = {
+  song: "그대 입술이",
+  singer: "장범준",
+};
+
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
