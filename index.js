8b5c891f71b93f1ff1e76191044a4e253bf9b105
uoohoon <hoonixox@naver.com>
2024-01-12 16:04:26 +0900

    feat: union type, any, unknown

--- index.js
+++ index.js
@@ -10,3 +10,20 @@ var project = {
     days: 30,
     started: true,
 };
+var 회원들 = [1, '2', 3];
+var 오브젝트 = { a: 123 };
+var 성 = 'kim';
+성 = 123;
+성 = undefined;
+성 = [];
+var user = 'kim';
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
--- index.ts
+++ index.ts
@@ -12,3 +12,24 @@ let project: { member: string[]; days: number; started: boolean } = {
   days: 30,
   started: true,
 };
+
+let 회원들 :(number | string)[] = [1, '2', 3]
+let 오브젝트 :{ a : string | number } = { a : 123 }
+
+let 성 :any = 'kim'
+성 = 123;
+성 = undefined;
+성 = []
+
+let user :string = 'kim';
+let age :(undefined | number) = undefined;
+let married :boolean = false; 
+let 철수 :(string|undefined|number|boolean)[]= [user, age, married];
+
+let 학교 :{ score : (number|boolean)[], teacher : string, friend : string|string[]} = {
+  score : [100, 97, 84],
+  teacher : 'Phil',
+  friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee' , 학교.teacher]
